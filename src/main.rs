1fe63059ac0c923686901731688436c076f56d99
Justin Lee <justinlee0022@gmail.com>
2020-02-12 11:36:46 -0800

    Working on rust grep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
