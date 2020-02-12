10ca531026034c8b46b15347fe687f0bb96a960e
Justin Lee <justinlee0022@gmail.com>
2020-02-12 14:05:54 -0800

    Added lowercase function

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
@@ -50,5 +66,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   contents.lines()
     .filter(|line| line.contains(query))
+    .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  contents.lines()
+    .filter(|line| line.to_lowercase().contains(&query))
     .collect()
 }
